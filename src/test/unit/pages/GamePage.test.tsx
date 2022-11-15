b65fb10ff2746119c4d69bfc5813ce5c172339d2
ksadoff <ksadoff20@gmail.com>
2022-11-14 21:39:34 -0500

    GB-24 created tests

---
+++ src/test/unit/pages/GamePage.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { GamePage } from '../../../pages/GamePage';
+
+describe('Rendering GamePage', () => {
+  beforeEach(() => {
+    render(<GamePage />);
+  });
+
+  it('renders game title', () => {
+    const title = screen.getByText(/Knights of the Old Republic/i);
+    expect(title).toBeInTheDocument();
+  })
+});
