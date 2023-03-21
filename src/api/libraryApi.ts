3b19bee635f94813a8e72ab83ed54f59c7711f19
ksadoff <ksadoff20@gmail.com>
2023-03-20 22:10:53 -0400

    code review feedback

--- src/api/libraryApi.ts
+++ src/api/libraryApi.ts
@@ -1,22 +1,23 @@
+import { homeUrl, librariesBaseUrl } from "../constants/ApiConstants";
 import Library from "../interfaces/Library";
 
 // TODO: will we eventually want this to be get all libraries for a single user? Will have to update backend endpoints
 export function getAllLibraries(): Promise<Library[]> {
-    return fetch("http://localhost:8080/libraries/", {
+    return fetch(homeUrl + librariesBaseUrl, {
         method: "GET",
     })
     .then((response) => response.json());
 }
 
 export function getLibrary(id: string): Promise<Library> {
-    return fetch(`http://localhost:8080/libraries/${id}/`, {
+    return fetch(homeUrl + librariesBaseUrl + `${id}/`, {
         method: "GET",
     })
     .then((response) => response.json());
 }
 
 export function createLibrary(library: Library): Promise<Library> {
-    return fetch("http://localhost:8080/libraries/", {
+    return fetch(homeUrl + librariesBaseUrl, {
         method: "POST",
         body: JSON.stringify(library),
     })
---
+++ src/constants/ApiConstants.ts
@@ -0,0 +1,3 @@
+export const homeUrl = "http://localhost:8080/";
+
+export const librariesBaseUrl = "libraries/";
