4a3c7b98ed332b9c768aec1bcceb7837df1bb103
ksadoff <ksadoff20@gmail.com>
2023-05-08 22:14:54 -0400

    adding routing, need tests

--- src/pages/LibraryPage.tsx
+++ src/pages/LibraryPage.tsx
@@ -4,10 +4,13 @@ import Library from "../interfaces/Library";
 import Game from "../interfaces/Game";
 import * as libraryApi from "../api/libraryApi";
 
+interface LibraryPageProps {
+    libraryId: string;
+}
 
 /*The page representing a specific library (e.g. Completed Games) */
 // TODO: How will we fetch libraryId? Query params? Context?
-export default function LibraryPage(libraryId: string) {
+export default function LibraryPage({ libraryId }: LibraryPageProps) {
     const [userLibraries, setUserLibraries] = useState<Array<Library>>([]);
     const [currentLibrary, setCurrentLibrary] = useState<Library>();
     const [currentGames, setCurrentGames] = useState<Array<Game>>([]);
