6df1f9ea722fe067b81cdf9e891c49b208e54c10
ksadoff <ksadoff20@gmail.com>
2023-07-03 22:48:14 -0400

    remove unnecessary change

--- src/test/unit/pages/LibrariesPage.test.tsx
+++ src/test/unit/pages/LibrariesPage.test.tsx
@@ -6,74 +6,72 @@ import LibraryRequest from '../../../interfaces/LibraryRequest';
 const getLibrariesStub = jest.spyOn(libraryApi, 'getAllLibrariesWithGames');
 const createLibraryStub = jest.spyOn(libraryApi, 'createLibrary');
 
+let setup = () => {
+  getLibrariesStub.mockResolvedValue([{id: "123",
+    name: "Want to Play",
+    games: [{
+        id: "123",
+        name: "Disco Elysium",
+        platforms: ["PC"],
+        genres: ["RPG"],
+        franchises: [],
+        companies: ["ZA/UM"],
+        releaseDate: ["2019-10-15"],
+        summary: "Kim Kitsuragi is the character ever.",
+        images: ["de.png"]}],
+    createDate: new Date()}])
+
+    act(() => {
+      render(<LibrariesPage />)
+  });
+};
+
 describe('Rendering LibrariesPage', () => {
-  let setup = () => {
-    getLibrariesStub.mockResolvedValue([{id: "123",
-      name: "Want to Play",
-      games: [{
-          id: "123",
-          name: "Disco Elysium",
-          platforms: ["PC"],
-          genres: ["RPG"],
-          franchises: [],
-          companies: ["ZA/UM"],
-          releaseDate: ["2019-10-15"],
-          summary: "Kim Kitsuragi is the character ever.",
-          images: ["de.png"]}],
-      createDate: new Date()}])
+  beforeEach(async () => {
+    setup()
+  });
 
-      act(() => {
-        render(<LibrariesPage />)
-    });
-  };
+  it('renders page title', () => {
+    const title = screen.getByText(/Libraries/i);
+    expect(title).toBeInTheDocument();
+  });
 
-  describe('Rendering LibrariesPage', () => {
-    beforeEach(async () => {
-      setup()
-    });
-
-    it('renders page title', () => {
-      const title = screen.getByText(/Libraries/i);
+  it('renders data', async () => {
+      await waitFor(() => {
+      const title = screen.getByText(/Want to Play/i)
       expect(title).toBeInTheDocument();
+      expect(screen.getByRole('link', { name: "Want to Play"})).toBeInTheDocument();
+      });
+      const game = screen.getByText(/Disco Elysium/i);
+      expect(game).toBeInTheDocument();
+  });
+
+  it('renders create new library button', () => {
+    expect(screen.getByText("Create Library"));
+  });
+
+  describe('when you click on the button', () => {
+    beforeEach(() => {
+      fireEvent.click(screen.getByText("Create Library"));
     });
 
-    it('renders data', async () => {
-        await waitFor(() => {
-        const title = screen.getByText(/Want to Play/i)
-        expect(title).toBeInTheDocument();
-        expect(screen.getByRole('link', { name: "Want to Play"})).toBeInTheDocument();
-        });
-        const game = screen.getByText(/Disco Elysium/i);
-        expect(game).toBeInTheDocument();
+    it('the modal appears', () => {
+      expect(screen.getByText("Enter your library name here"));
     });
 
-    it('renders create new library button', () => {
-      expect(screen.getByText("Create Library"));
-    });
-
-    describe('when you click on the button', () => {
+    describe('and when we submit a new library', () => {
       beforeEach(() => {
-        fireEvent.click(screen.getByText("Create Library"));
+        const input = screen.getByTestId("libraryName");
+        fireEvent.change(input, { target: { value: "New Library" } });
+        fireEvent.click(screen.getByText("submit"));
       });
 
-      it('the modal appears', () => {
-        expect(screen.getByText("Enter your library name here"));
-      });
-
-      describe('and when we submit a new library', () => {
-        beforeEach(() => {
-          const input = screen.getByTestId("libraryName");
-          fireEvent.change(input, { target: { value: "New Library" } });
-          fireEvent.click(screen.getByText("submit"));
-        });
-
-        it('calls the create library api', () => {
-          const libraryToCreate: LibraryRequest = {
-            name: "New Library",
-            games: [],
-        };
-          expect(createLibraryStub).toHaveBeenCalledWith(libraryToCreate);
-        });
+      it('calls the create library api', () => {
+        const libraryToCreate: LibraryRequest = {
+          name: "New Library",
+          games: [],
+      };
+        expect(createLibraryStub).toHaveBeenCalledWith(libraryToCreate);
       });
     });
   });
