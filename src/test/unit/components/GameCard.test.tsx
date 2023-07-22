6db0a8c886d02421f16bbc00a73c57254d0b037a
Tracy Goldman <tag96@cornell.edu>
2023-07-21 20:08:35 -0400

    Added unit tests

--- src/test/unit/components/GameCard.test.tsx
+++ src/test/unit/components/GameCard.test.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {fireEvent, render, screen} from '@testing-library/react';
+import selectEvent from 'react-select-event'
 import { GameCard } from '../../../components/GameCard';
 import * as libraryApi from "../../../api/libraryApi";
 import Library from "../../../interfaces/Library";
+import Game from "../../../interfaces/Game"
 
 describe('Rendering Game GameCard', () => {
 
   const allLibrariesStub = jest.spyOn(libraryApi, 'getAllLibraries')
-  const addToLibraryStub = jest.spyOn(libraryApi, "addToLibrary")
+  const addToLibraryStub = jest.spyOn(libraryApi, 'addToLibrary')
 
   const library1: Library = {
     id: "1",
@@ -34,10 +36,28 @@ describe('Rendering Game GameCard', () => {
     createDate: new Date()
   }
 
+  const mockGame: Game = {
+    companies: [""],
+    franchises: [""],
+    genres: [""],
+    images: [""],
+    name: "",
+    platforms: [""],
+    releaseDate: [""],
+    summary: "",
+    id: ""
+  }
+
   let getMockLibraries = () => [library1, library2, library3, library4]
 
   let setup = () => {
     allLibrariesStub.mockResolvedValue(getMockLibraries())
+    addToLibraryStub.mockImplementation((gameId: string, libraryId: string): Promise<any> => {
+      mockGame.id = gameId
+      const libraryInd = +libraryId - 1
+      getMockLibraries()[libraryInd].games.push(mockGame)
+      return Promise.resolve()
+    })
     render(
         <GameCard
             gameId = "testID"
@@ -89,7 +109,50 @@ describe('Rendering Game GameCard', () => {
   })
 
   it('renders add to library button', async () => {
-    const selectPlaceholder = screen.getByText(/Add To Library/);
-    expect(selectPlaceholder).toBeInTheDocument()
+    const addToLibraryPlaceholder = screen.getByText(/Add to Library/);
+    expect(addToLibraryPlaceholder).toBeInTheDocument()
+  })
+
+  it('should add game to library after clicking library', async () => {
+    await selectEvent.select(screen.getByText(/Select a Library/), "Want to Play")
+    fireEvent(
+        screen.getByText('Add to Library'),
+        new MouseEvent('click', {
+          bubbles: true,
+          cancelable: true,
+        }),
+    )
+    expect(library1.games.length).toEqual(1)
+    expect(library1.games[0].id).toEqual("testID")
+  })
+
+  it('should not add to library with no games selected', async () => {
+    fireEvent(
+        screen.getByText('Add to Library'),
+        new MouseEvent('click', {
+          bubbles: true,
+          cancelable: true,
+        }),
+    )
+    expect(addToLibraryStub).not.toHaveBeenCalled()
+    expect(library1.games.length).toEqual(0)
+    expect(library2.games.length).toEqual(0)
+    expect(library3.games.length).toEqual(0)
+    expect(library4.games.length).toEqual(0)
+  })
+
+  it("should select one library, then another, and add to the second library", async () => {
+    await selectEvent.select(screen.getByText(/Select a Library/), "Wishlist")
+    await selectEvent.select(screen.getByText(/Wishlist/), "Games Under 5 Hours")
+    fireEvent(
+        screen.getByText('Add to Library'),
+        new MouseEvent('click', {
+          bubbles: true,
+          cancelable: true,
+        }),
+    )
+    expect(library3.games.length).toEqual(0)
+    expect(library4.games.length).toEqual(1)
+    expect(library4.games[0].id).toEqual("testID")
   })
 });
