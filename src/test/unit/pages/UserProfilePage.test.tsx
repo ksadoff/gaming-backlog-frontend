816cee17ef654eb9e09699005cedca190c6ebdb2
Katie Sadoff <ksadoff20@gmail.com>
2023-09-04 22:31:56 -0400

    cleanup

--- src/test/unit/pages/UserProfilePage.test.tsx
+++ src/test/unit/pages/UserProfilePage.test.tsx
@@ -9,6 +9,7 @@ const mockUser = {
     password: 'what is security',
     createdDate: '2023-08-28',
   };
+  
 const mockGetUser = jest.spyOn(userApi, 'getUser');
 
 describe('UserProfilePage', () => {
@@ -16,8 +17,9 @@ describe('UserProfilePage', () => {
     await act(async () => {
       mockGetUser.mockResolvedValue(mockUser);
       render(<UserProfilePage id={"123"}/>);
-    })
-  })
+    });
+  });
+
   describe('when rendered', () => {
     it('displays user profile information', async () => {
       const displayNameElement = await screen.findByText(mockUser.displayName);
@@ -25,29 +27,27 @@ describe('UserProfilePage', () => {
   
       expect(displayNameElement).toBeInTheDocument();
       expect(emailElement).toBeInTheDocument();
-    })
+    });
 
-  describe('when change password is initiated', () => {
-    it('changes password', () => {
-      const currentPasswordInput = screen.getByLabelText(/Current Password:/);
-      const newPasswordInput = screen.getAllByLabelText(/New Password:/)[0];
-      const confirmNewPasswordInput = screen.getByLabelText(/Confirm New Password:/);
-      const changePasswordButton = screen.getByRole('button', { name: "Change Password" });
+    describe('when change password is initiated', () => {
+      it('changes password', () => {
+        const currentPasswordInput = screen.getByLabelText(/Current Password:/);
+        const newPasswordInput = screen.getAllByLabelText(/New Password:/)[0];
+        const confirmNewPasswordInput = screen.getByLabelText(/Confirm New Password:/);
+        const changePasswordButton = screen.getByRole('button', { name: "Change Password" });
 
-      expect(currentPasswordInput).toBeInTheDocument();
-      expect(newPasswordInput).toBeInTheDocument();
-      expect(confirmNewPasswordInput).toBeInTheDocument();
-      expect(changePasswordButton).toBeInTheDocument();
-   
-      // TODO: Test actual functionality
-      // Simulate user input
-      // fireEvent.change(currentPasswordInput, { target: { value: 'oldpassword' } });
-      // fireEvent.change(newPasswordInput, { target: { value: 'newpassword' } });
-      // fireEvent.change(confirmNewPasswordInput, { target: { value: 'newpassword' } });
-  
-      // fireEvent.click(changePasswordButton);
+        expect(currentPasswordInput).toBeInTheDocument();
+        expect(newPasswordInput).toBeInTheDocument();
+        expect(confirmNewPasswordInput).toBeInTheDocument();
+        expect(changePasswordButton).toBeInTheDocument();
+    
+        // TODO: Test actual functionality
+        // Simulate user input
+        // fireEvent.change(currentPasswordInput, { target: { value: 'oldpassword' } });
+        // fireEvent.change(newPasswordInput, { target: { value: 'newpassword' } });
+        // fireEvent.change(confirmNewPasswordInput, { target: { value: 'newpassword' } });
+        // fireEvent.click(changePasswordButton);
+      });
     });
   });
-});
-
 });
