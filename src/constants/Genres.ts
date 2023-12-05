898e876e23310d6713b353fcf4b1d132bdf35b51
Katie Sadoff <ksadoff20@gmail.com>
2023-12-04 22:18:51 -0500

    added all IGDB values

---
+++ src/constants/Genres.ts
@@ -0,0 +1,26 @@
+export enum Genres {
+    PointAndClick = "Point-and-click",
+    Fighting = "Fighting",
+    Shooter = "Shooter",
+    Music = "Music",
+    Platform = "Platform",
+    Puzzle = "Puzzle",
+    Racing = "Racing",
+    RealTimeStrategy = "Real Time Strategy (RTS)",
+    RolePlaying = "Role-playing (RPG)",
+    Simulator = "Simulator",
+    Sport = "Sport",
+    Strategy = "Strategy",
+    TurnBasedStrategy = "Turn Based Strategy (TBS)",
+    Tactical = "Tactical",
+    Trivia = "Quiz/Trivia",
+    HackAndSlashBeatEmUp = "Hack and slash/Beat 'em up",
+    Pinball = "Pinball",
+    Adventure = "Adventure",
+    Arcade = "Arcade",
+    VisualNovel = "Visual Novel",
+    Indie = "Indie",
+    CardAndBoardGame = "Card & Board Game",
+    MOBA = "MOBA"
+    
+}
