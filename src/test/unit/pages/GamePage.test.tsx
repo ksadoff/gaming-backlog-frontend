1f5bfd62422988077f1e966607ea06227be08004
Katie Sadoff <ksadoff20@gmail.com>
2024-02-24 17:38:02 -0500

    actually this time

--- src/test/unit/pages/GamePage.test.tsx
+++ src/test/unit/pages/GamePage.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, act } from '@testing-library/react';
+import { render, screen, act, waitFor } from '@testing-library/react';
 import GamePage  from '../../../pages/GamePage';
 import Game from '../../../interfaces/Game';
 import * as gameApi from '../../../api/gameApi';
@@ -17,58 +17,75 @@ describe('Rendering GamePage', () => {
     images: ["img"]
 }
 
-let setup = () => {
+let setup = async () => {
   const mockGetGame = jest.spyOn(gameApi, 'getGame')
   mockGetGame.mockResolvedValue(mockGame)
-  render(<GamePage gameId="test" />);
+
+  await act(async () => {
+    render(<GamePage gameId="test" />);
+  })
 }
 
 beforeEach(async() => {
-  act(() => {
-    setup();
-  })
+  await setup();
 })
 
   it('renders game title', async () => {
-    const title = await screen.findByText(/The Legend of Zelda: Tears of the Kingdom/);
-    expect(title).toBeInTheDocument();
+    await waitFor(async () => {
+      const title = await screen.findByText(/The Legend of Zelda: Tears of the Kingdom/);
+      expect(title).toBeInTheDocument();
+    })
   })
 
   it('renders platforms', async () => {
-    const platform = await screen.findByText(/Nintendo Switch/);
-    expect(platform).toBeInTheDocument();
+    await waitFor(async () => {
+      const platform = await screen.findByText(/Nintendo Switch/);
+      expect(platform).toBeInTheDocument();
+    });
   })
 
   it('renders genres', async () => {
-    const genre1 = await screen.findByText(/Action-Adventure/);
-    expect(genre1).toBeInTheDocument();
+    await waitFor(async () => {
+      const genre1 = await screen.findByText(/Action-Adventure/);
+      expect(genre1).toBeInTheDocument();
 
-    const genre2 = await screen.findAllByText(/Adventure/);
-    expect(genre2[1]).toBeInTheDocument();
+      const genre2 = await screen.findAllByText(/Adventure/);
+      expect(genre2[1]).toBeInTheDocument();
+    })
   })
 
   it('renders franchises', async () => {
-    const franchise = await screen.findAllByText(/Legend of Zelda/);
-    expect(franchise[1]).toBeInTheDocument();
+    await waitFor(async () => {
+      const franchise = await screen.findAllByText(/Legend of Zelda/);
+      expect(franchise[1]).toBeInTheDocument();
+    });
   })
 
   it('renders comapnies', async () => {
-    const company = await screen.findAllByText(/Nintendo/);
-    expect(company[1]).toBeInTheDocument();
+    await waitFor(async () => {
+      const company = await screen.findAllByText(/Nintendo/);
+      expect(company[1]).toBeInTheDocument();
+    })
   })
 
   it('renders release date', async () => {
-    const date = await screen.findByText(/May 12 2023/i);
-    expect(date).toBeInTheDocument();
+    await waitFor(async () => {
+      const date = await screen.findByText(/May 12 2023/i);
+      expect(date).toBeInTheDocument();
+    })
   })
 
   it('renders summary', async () => {
-    const summary = await screen.findByText(/So much Zelda so much fun/i);
-    expect(summary).toBeInTheDocument();
+    await waitFor(async () => {
+      const summary = await screen.findByText(/So much Zelda so much fun/i);
+      expect(summary).toBeInTheDocument();
+    })
   })
 
   it('renders image', async () => {
-    const image = await screen.findByAltText("Game")
-    expect(image).toBeInTheDocument();
+    await waitFor(async () => {
+      const image = await screen.findByAltText("Game")
+      expect(image).toBeInTheDocument();
+    })
   })
 });
