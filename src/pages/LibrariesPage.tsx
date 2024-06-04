9e5992aae27a5800967b6c6058247bb7e487263e
Katie Sadoff <ksadoff20@gmail.com>
2024-06-03 21:59:48 -0400

    Removed unnecessary space

--- src/pages/LibrariesPage.tsx
+++ src/pages/LibrariesPage.tsx
@@ -83,7 +83,7 @@ function sortLibraries() {
 
     return(
         <>
-            <h1 >
+            <h1>
                 Libraries 
                 <a data-testid="profile" href="/users/64fcee6c18bc4d16a9f2051e" style={{marginLeft: "10px"}}> 
                     <button>Profile</button>
